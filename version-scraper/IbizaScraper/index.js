00c2231eca26ac3c85cbbd0f19480f8841fe8d42
Travis Harris <travisharris@Traviss-MacBook-Pro.local>
2018-11-29 16:04:55 -0800

    keep ibiza on old db version

--- version-scraper/IbizaScraper/index.js
+++ version-scraper/IbizaScraper/index.js
@@ -17,7 +17,7 @@ module.exports = async function(context, myTimer) {
     }));
   const db = await MongoClient.connect(url);
   try {
-    const dbo = db.db("versions2");
+    const dbo = db.db("versions");
     const p = versionFile.map(async v => {
       var query = { name: v.name };
       const lastInsertedVersion = await dbo
