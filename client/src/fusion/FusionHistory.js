699d48626674dc487e0e06181ed475869945a3aa
Travis Harris <travisharris@traviss-mbp.lan>
2018-12-03 21:58:52 -0800

    add search functionality to fusion history

--- client/src/fusion/FusionHistory.js
+++ client/src/fusion/FusionHistory.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import { Query } from "react-apollo";
 import gql from "graphql-tag";
 import { Timeline, TimelineEvent } from "react-event-timeline";
@@ -8,7 +8,10 @@ import { ActionButton } from "office-ui-fabric-react/lib/Button";
 import { Breadcrumb } from "office-ui-fabric-react/lib/Breadcrumb";
 import { navigate } from "@reach/router";
 import LoadingPage from "../LoadingPage";
+import { SearchBox } from "office-ui-fabric-react/lib/SearchBox";
 export default function FusionHistory(props) {
+  const [searchTerm, setSearchTerm] = useState(null);
+
   const onBuildClick = buildNumber => {
     const versionSplit = buildNumber.split(".");
     const bn = versionSplit[versionSplit.length - 1];
@@ -33,6 +36,11 @@ export default function FusionHistory(props) {
           { text: "Home", key: "f1", onClick: onNavHome },
           { text: `${props.loc} history`, key: "f2" }
         ]}
+      />
+      <SearchBox
+        placeholder="Filter"
+        onChanged={newValue => setSearchTerm(newValue.toLowerCase())}
+        iconProps={{ iconName: "Filter" }}
       />
       <Query
         query={gql`
@@ -76,62 +84,92 @@ export default function FusionHistory(props) {
 
           return (
             <Timeline>
-              {versionHistory.map(version => (
-                <TimelineEvent
-                  titleStyle={{ fontWeight: "bold", fontSize: "14px" }}
-                  createdAtStyle={{ fontWeight: "bold", fontSize: "14px" }}
-                  title={`Version: ${version.version}`}
-                  createdAt={moment
-                    .utc(version.timeStamp)
-                    .local()
-                    .format("YYYY-MM-DD hh:mm a")}
-                  icon={<Icon iconName="vstslogo" />}
-                >
-                  {version.githubCommitData && (
-                    <div>
-                      <h3 style={{ marginBottom: "5px" }}>Commits: </h3>
-                      <ul
-                        style={{
-                          fontSize: ".8rem",
-                          marginLeft: "20px",
-                          listStyleType: "square"
-                        }}
-                      >
-                        {version.githubCommitData.commits.map(commit => (
-                          <li>
-                            {commit.commit.message} by{" "}
-                            <b>{commit.commit.author.name}</b>
-                            <a
-                              href={`https://github.com/azure/azure-functions-ux/commit/${
-                                commit.sha
-                              }`}
-                              target="_blank"
+              {versionHistory
+                .filter(x => {
+                  if (!searchTerm) return true;
+                  if (!x.githubCommitData) return false;
+                  const commitMessages = x.githubCommitData.commits.map(y =>
+                    y.commit.message.toLowerCase()
+                  );
+                  const commitAuthors = x.githubCommitData.commits.map(y =>
+                    y.commit.author.name.toLowerCase()
+                  );
+                  console.log(commitAuthors, searchTerm);
+                  return (
+                    !!commitMessages.find(x => !!x && x.includes(searchTerm)) ||
+                    !!commitAuthors.find(x => !!x && x.includes(searchTerm))
+                  );
+                })
+                .map(version => (
+                  <TimelineEvent
+                    key={version.timeStamp}
+                    titleStyle={{ fontWeight: "bold", fontSize: "14px" }}
+                    createdAtStyle={{ fontWeight: "bold", fontSize: "14px" }}
+                    title={`Version: ${version.version}`}
+                    createdAt={moment
+                      .utc(version.timeStamp)
+                      .local()
+                      .format("YYYY-MM-DD hh:mm a")}
+                    icon={<Icon iconName="vstslogo" />}
+                  >
+                    {version.githubCommitData && (
+                      <div>
+                        <h3 style={{ marginBottom: "5px" }}>Commits: </h3>
+                        <ul
+                          style={{
+                            fontSize: ".8rem",
+                            marginLeft: "20px",
+                            listStyleType: "square"
+                          }}
+                        >
+                          {version.githubCommitData.commits.map(commit => (
+                            <li
+                              style={{
+                                opacity:
+                                  !!searchTerm &&
+                                  !commit.commit.message
+                                    .toLowerCase()
+                                    .includes(searchTerm) &&
+                                  !commit.commit.author.name
+                                    .toLowerCase()
+                                    .includes(searchTerm)
+                                    ? 0.3
+                                    : 1
+                              }}
                             >
-                              {commit.sha}
-                            </a>
-                          </li>
-                        ))}
-                      </ul>
-                    </div>
-                  )}
-                  <ActionButton
-                    iconProps={{ iconName: "link" }}
-                    allowDisabledFocus={true}
-                    onClick={() =>
-                      onCommitClick(version.githubCommitData.permalink_url)
-                    }
-                  >
-                    See changes from last release
-                  </ActionButton>
-                  <ActionButton
-                    iconProps={{ iconName: "link" }}
-                    allowDisabledFocus={true}
-                    onClick={() => onBuildClick(version.version)}
-                  >
-                    Go To Build
-                  </ActionButton>
-                </TimelineEvent>
-              ))}
+                              {commit.commit.message} by{" "}
+                              <b>{commit.commit.author.name}</b>
+                              <a
+                                href={`https://github.com/azure/azure-functions-ux/commit/${
+                                  commit.sha
+                                }`}
+                                target="_blank"
+                              >
+                                {commit.sha}
+                              </a>
+                            </li>
+                          ))}
+                        </ul>
+                      </div>
+                    )}
+                    <ActionButton
+                      iconProps={{ iconName: "link" }}
+                      allowDisabledFocus={true}
+                      onClick={() =>
+                        onCommitClick(version.githubCommitData.permalink_url)
+                      }
+                    >
+                      See changes from last release
+                    </ActionButton>
+                    <ActionButton
+                      iconProps={{ iconName: "link" }}
+                      allowDisabledFocus={true}
+                      onClick={() => onBuildClick(version.version)}
+                    >
+                      Go To Build
+                    </ActionButton>
+                  </TimelineEvent>
+                ))}
             </Timeline>
           );
         }}
